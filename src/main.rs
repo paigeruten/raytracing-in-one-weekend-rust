d40aceefdc336ea1c214b50c7f0c99ed70dda0e7
Pailey Quilts <paileyq@gmail.com>
2020-12-12 12:50:06 -0600

    Chapter 4

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
 mod color;
+mod ray;
 mod vec3;
 
-use crate::vec3::Color;
 use crate::color::write_color;
+use crate::ray::Ray;
+use crate::vec3::{Color, Point3, Vec3};
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = r.direction().unit_vector();
+    let t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*Color::new(1.0, 1.0, 1.0) + t*Color::new(0.5, 0.7, 1.0);
+}
 
 fn main() {
     // Image
 
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
+
+    // Camera
+
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     // Render
 
@@ -21,11 +41,10 @@ fn main() {
     for y in (0..image_height).rev() {
         eprint!("\rScanlines remaining: {} ", y);
         for x in 0..image_width {
-            let pixel_color = Color::new(
-                x as f64 / (image_width-1) as f64,
-                y as f64 / (image_height-1) as f64,
-                0.25,
-            );
+            let u = x as f64 / (image_width as f64 - 1.0);
+            let v = y as f64 / (image_height as f64 - 1.0);
+            let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            let pixel_color = ray_color(&r);
             write_color(&mut stdout, pixel_color);
         }
     }
---
+++ src/ray.rs
@@ -0,0 +1,24 @@
+use crate::vec3::{Point3, Vec3};
+
+pub struct Ray {
+    orig: Point3,
+    dir: Vec3,
+}
+
+impl Ray {
+    pub fn new(orig: Point3, dir: Vec3) -> Self {
+        Ray { orig, dir }
+    }
+
+    pub fn origin(&self) -> Point3 {
+        self.orig
+    }
+
+    pub fn direction(&self) -> Vec3 {
+        self.dir
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        self.orig + t*self.dir
+    }
+}
