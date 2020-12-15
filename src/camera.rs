b713681b910dbe7975fc76992d258512f931197b
Pailey Quilts <paileyq@gmail.com>
2020-12-14 23:53:30 -0600

    Chapter 11

--- src/camera.rs
+++ src/camera.rs
@@ -8,21 +8,25 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new() -> Self {
-        let aspect_ratio = 16.0 / 9.0;
-        let viewport_height = 2.0;
+    pub fn new(lookfrom: Point3, lookat: Point3, vup: Vec3, vfov: f64, aspect_ratio: f64) -> Self {
+        let theta = vfov.to_radians();
+        let h = (theta/2.0).tan();
+        let viewport_height = 2.0 * h;
         let viewport_width = aspect_ratio * viewport_height;
-        let focal_length = 1.0;
 
-        let origin = Point3::new(0.0, 0.0, 0.0);
-        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-        let vertical = Vec3::new(0.0, viewport_height, 0.0);
-        let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3::new(0.0, 0.0, focal_length);
+        let w = (lookfrom - lookat).unit_vector();
+        let u = vup.cross(w).unit_vector();
+        let v = w.cross(u);
+
+        let origin = lookfrom;
+        let horizontal = viewport_width * u;
+        let vertical = viewport_height * v;
+        let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - w;
 
         Camera { origin, lower_left_corner, horizontal, vertical }
     }
 
-    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
-        Ray::new(self.origin, self.lower_left_corner + u*self.horizontal + v*self.vertical - self.origin)
+    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
+        Ray::new(self.origin, self.lower_left_corner + s*self.horizontal + t*self.vertical - self.origin)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -49,19 +49,25 @@ fn main() {
 
     let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Rc::new(Dielectric::new(1.5));
-    let material_left_inner = Rc::new(Dielectric::new(1.5));
-    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
+    let material_left   = Rc::new(Dielectric::new(1.5));
+    let material_left2  = Rc::new(Dielectric::new(1.5)); // can't figure out how to reuse material_left :(
+    let material_right  = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
 
     world.add(Rc::new(Sphere::new(Point3::new( 0.0, -100.5, -1.0), 100.0, material_ground)));
     world.add(Rc::new(Sphere::new(Point3::new( 0.0,    0.0, -1.0),   0.5, material_center)));
     world.add(Rc::new(Sphere::new(Point3::new(-1.0,    0.0, -1.0),   0.5, material_left)));
-    world.add(Rc::new(Sphere::new(Point3::new(-1.0,    0.0, -1.0),  -0.4, material_left_inner)));
+    world.add(Rc::new(Sphere::new(Point3::new(-1.0,    0.0, -1.0), -0.45, material_left2)));
     world.add(Rc::new(Sphere::new(Point3::new( 1.0,    0.0, -1.0),   0.5, material_right)));
 
     // Camera
 
-    let cam = Camera::new();
+    let cam = Camera::new(
+        Point3::new(-2.0, 2.0, 1.0),
+        Point3::new(0.0, 0.0, -1.0),
+        Vec3::new(0.0, 1.0, 0.0),
+        20.0,
+        aspect_ratio
+    );
 
     // Render
 
